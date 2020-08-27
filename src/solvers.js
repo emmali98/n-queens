56601ef57b451aae1512eb4529d9a5b5a0458e53
Emma Li <emma.m.li.98@gmail.com>
2020-08-26 20:15:42 -0700

    Add clearer pseudocode, structure for solution helper functions

--- src/solvers.js
+++ src/solvers.js
@@ -20,11 +20,37 @@ For each potential child, check if it has any conflicts before adding
 Recursively add children by toggling on spaces in latter half of matrix
 Grab all the children at the bottom
 */
-window._Tree = function() { };
+window._Tree = function(arr) {
+  this.board = arr;
+  this.children = [];
+};
 
 window._solutionHelper = function(n, isQueens) {
+  // make top of the tree
+  var solutionTree = new window._Tree(new Array(n * n).fill(0));
   // check isQueens to determine if we check diagonals too
-  return solutions;
+  var addChildren = function(tree) {
+    // looks at tree.board and searches for indexOf the last occurring 1
+    var idx = tree.board.lastIndexOf(1) === -1 ? 0 : (tree.board.lastIndexOf(1) + 1);
+    // if idx is n^2 - 1, return tree
+    if (idx === (n * n - 1)) {
+      return tree;
+    } else {
+      // for loop (i = idx; i < n^2; i++)
+      //   childBoard = tree.board.slice(), childBoard[i] changed from 0 to 1
+      //   check childBoard for conflicts, change back to matrix for checks, then re-flatten
+      //   childBoard = addChildren(childBoard);
+      //   tree.children.push(childBoard);
+
+      // generate n^2-(n-1) new matrices that have one place toggled on
+      // check these matrices, then add to tree.children
+      // call addChildren to each of tree.children
+    }
+
+  };
+  solutionTree = addChildren(solutionTree);
+  // var solutions = do something to solutionTree to grab bottom children with n pieces (need to check)
+  return solutionTree;
 };
 
 window.findNRooksSolution = function(n) {
@@ -56,4 +82,12 @@ window.countNQueensSolutions = function(n) {
 
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
+};
+
+var makeEmptyMatrix = function(n) {
+  return _(_.range(n)).map(function() {
+    return _(_.range(n)).map(function() {
+      return 0;
+    });
+  });
 };
